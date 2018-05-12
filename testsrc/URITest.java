5068d8dd9783255f3fa4fea65affa8ae23887fc5
David Creigh <thistleBgood@users.noreply.github.com>
2018-05-12 20:47:56 +0100

    Added extra test samples

--- testsrc/URITest.java
+++ testsrc/URITest.java
@@ -10,12 +10,24 @@ public class URITest {
 
     private static final String SCHEME = "scheme://authority/path?query#fragment";
     private static final String HTTPS = "https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top";
+    private static final String LDAP = "ldap://[2001:db8::7]/c=GB?objectClass?one";
+    private static final String MAILTO = "mailto:John.Doe@example.com";
+    private static final String NEWS = "news:comp.infosystems.www.servers.unix";
+    private static final String TEL = "tel:+1-816-555-1212";
+    private static final String TELNET = "telnet://192.0.2.16:80/";
+    private static final String URN = "urn:oasis:names:specification:docbook:dtd:xml:4.1.2";
 
     @Test
     public void uri_extracts_scheme() {
         Map<String, String> items = new HashMap<>();
         items.put(SCHEME, "scheme");
         items.put(HTTPS, "https");
+        items.put(LDAP, "ldap");
+        items.put(MAILTO, "mailto");
+        items.put(NEWS, "news");
+        items.put(TEL, "tel");
+        items.put(TELNET, "telnet");
+        items.put(URN, "urn");
 
         for(String uri : items.keySet()) {
             String scheme = items.get(uri);
