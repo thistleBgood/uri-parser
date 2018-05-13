abfba504a169acec95a7a8458753cfdbb704bf71
David Creigh <thistleBgood@users.noreply.github.com>
2018-05-14 00:11:33 +0100

    Updated builder to use URIData structure.

--- src/io/thistlebgood/uri/URIBuilder.java
+++ src/io/thistlebgood/uri/URIBuilder.java
@@ -4,55 +4,51 @@ import static io.thistlebgood.uri.URIConstants.*;
 import static io.thistlebgood.uri.URIUtils.optionalComponentIsPresent;
 
 public class URIBuilder {
-    private String fullUri;
-    private String scheme;
-    private String authority;
-    private String path;
-    private String query;
-    private String fragment;
+    private URIData uri;
 
     public URIBuilder(String scheme, String authority, String path, String query, String fragment) {
-        this.scheme = scheme;
-        this.authority = authority;
-        this.path = path;
-        this.query = query;
-        this.fragment = fragment;
+        this.uri = new URIData();
+        this.uri.scheme = scheme;
+        this.uri.authority = authority;
+        this.uri.path = path;
+        this.uri.query = query;
+        this.uri.fragment = fragment;
         toString();
     }
 
     @Override
     public String toString() {
         resetUri();
-        fullUri += buildScheme();
-        fullUri += buildAuthority();
-        fullUri += buildPath();
-        fullUri += buildQuery();
-        fullUri += buildFragment();
-        return fullUri;
+        this.uri.fullUri += buildScheme();
+        this.uri.fullUri += buildAuthority();
+        this.uri.fullUri += buildPath();
+        this.uri.fullUri += buildQuery();
+        this.uri.fullUri += buildFragment();
+        return this.uri.fullUri;
     }
 
     private void resetUri() {
-        fullUri = COMPONENT_IS_EMPTY;
+        this.uri.fullUri = COMPONENT_IS_EMPTY;
     }
 
     private String buildScheme() {
-        return scheme + SCHEME_MARKER;
+        return this.uri.scheme + SCHEME_MARKER;
     }
 
     private String buildAuthority() {
-        return buildOptionalComponent(AUTHORITY_MARKER, authority);
+        return buildOptionalComponent(AUTHORITY_MARKER, this.uri.authority);
     }
 
     private String buildPath() {
-        return path;
+        return this.uri.path;
     }
 
     private String buildQuery() {
-        return buildOptionalComponent(QUERY_MARKER, query);
+        return buildOptionalComponent(QUERY_MARKER, this.uri.query);
     }
 
     private String buildFragment() {
-        return buildOptionalComponent(FRAGMENT_MARKER, fragment);
+        return buildOptionalComponent(FRAGMENT_MARKER, this.uri.fragment);
     }
 
     private String buildOptionalComponent(String marker, String component) {
