c89133317451a52ec912759b3d6ecd3e331a4438
David Creigh <thistleBgood@users.noreply.github.com>
2018-05-14 20:20:34 +0100

    Inverted logic in another part of path parsing method.

--- src/io/thistlebgood/uri/URIParser.java
+++ src/io/thistlebgood/uri/URIParser.java
@@ -45,15 +45,15 @@ public class URIParser {
         String trimmedUri = this.uri.fullUri.substring(startMarker);
         int queryMarker = trimmedUri.indexOf(QUERY_MARKER);
 
-        if (queryMarker == -1) {
+        if (!(queryMarker == -1)) {
+            this.uri.path = cropToMarker(trimmedUri, QUERY_MARKER);
+        } else {
             int fragmentMarker = trimmedUri.indexOf(FRAGMENT_MARKER);
             if (indexFound(fragmentMarker)) {
                 this.uri.path = cropToMarker(trimmedUri, FRAGMENT_MARKER);
             } else {
                 this.uri.path = trimmedUri;
             }
-        } else {
-            this.uri.path = cropToMarker(trimmedUri, QUERY_MARKER);
         }
     }
 
