9e84cf4c495ba00e6d535bc12ed75da127a9fcdb
David Creigh <thistleBgood@users.noreply.github.com>
2018-05-14 21:44:33 +0100

    Extracted constant.

--- src/io/thistlebgood/uri/AuthorityParser.java
+++ src/io/thistlebgood/uri/AuthorityParser.java
@@ -1,6 +1,7 @@
 package io.thistlebgood.uri;
 
 import static io.thistlebgood.uri.URIConstants.AUTHORITY_MARKER;
+import static io.thistlebgood.uri.URIConstants.PATH_MARKER;
 import static io.thistlebgood.uri.URIConstants.COMPONENT_IS_EMPTY;
 import static io.thistlebgood.uri.URIUtils.cropToMarker;
 import static io.thistlebgood.uri.URIUtils.indexFound;
@@ -14,7 +15,7 @@ class AuthorityParser {
 
         if (indexFound(startMarker)) {
             String trimmedUri = fullUri.substring(startMarker + AUTHORITY_MARKER.length());
-            authority = cropToMarker(trimmedUri, "/");
+            authority = cropToMarker(trimmedUri, PATH_MARKER);
         }
 
         return authority;
--- src/io/thistlebgood/uri/URIConstants.java
+++ src/io/thistlebgood/uri/URIConstants.java
@@ -3,6 +3,7 @@ package io.thistlebgood.uri;
 public class URIConstants {
     public static final String SCHEME_MARKER = ":";
     public static final String AUTHORITY_MARKER = "//";
+    public static final String PATH_MARKER = "/";
     public static final String QUERY_MARKER = "?";
     public static final String FRAGMENT_MARKER = "#";
 
